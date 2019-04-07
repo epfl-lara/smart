3907f34573a087e99a4b29cf7d4766763b3fa370
Jad Hamza <jad.hamza@epfl.ch>
2019-04-07 10:35:53 +0200

    Restore SymbolIdentifier to upstream version

--- core/src/main/scala/stainless/ast/SymbolIdentifier.scala
+++ core/src/main/scala/stainless/ast/SymbolIdentifier.scala
@@ -1,51 +1,42 @@
 /* Copyright 2009-2018 EPFL, Lausanne */
 
 package stainless
+package ast
 
-package object ast {
+class Symbol private[stainless](val path: Seq[String], private[stainless] val id: Int) {
+  def this(name: String, id: Int) = this(name.split("\\.").toSeq, id)
 
-  class Symbol private[stainless](val path: Seq[String], private[stainless] val id: Int) {
-    def this(name: String, id: Int) = this(name.split("\\.").toSeq, id)
+  val name: String = path.mkString(".")
 
-    val name: String = path.mkString(".")
-
-    override def equals(that: Any): Boolean = that match {
-      case s: Symbol => id == s.id
-      case _ => false
-    }
-
-    override def hashCode: Int = id
+  override def equals(that: Any): Boolean = that match {
+    case s: Symbol => id == s.id
+    case _ => false
   }
 
-  object Symbol {
-    private val counter = new inox.utils.UniqueCounter[Unit]
+  override def hashCode: Int = id
+}
 
-    def apply(name: String) = new Symbol(name, counter.nextGlobal)
+object Symbol {
+  private val counter = new inox.utils.UniqueCounter[Unit]
+
+  def apply(name: String) = new Symbol(name, counter.nextGlobal)
+}
+
+class SymbolIdentifier private[stainless](id: Identifier, val symbol: Symbol)
+  extends Identifier(id.name, id.globalId, id.id, alwaysShowUniqueID = false)
+
+object SymbolIdentifier {
+  def apply(name: String): SymbolIdentifier = {
+    new SymbolIdentifier(FreshIdentifier(name.split("\\.").last), Symbol(name))
   }
 
-  class SymbolIdentifier private[stainless](id: Identifier, val symbol: Symbol)
-    extends Identifier(id.name, id.globalId, id.id, alwaysShowUniqueID = false)
-
-  object SymbolIdentifier {
-    def apply(name: String): SymbolIdentifier = {
-      new SymbolIdentifier(FreshIdentifier(name.split("\\.").last), Symbol(name))
-    }
-
-    def apply(sym: Symbol): SymbolIdentifier = {
-      new SymbolIdentifier(FreshIdentifier(sym.path.last), sym)
-    }
-
-    def unapply(id: SymbolIdentifier): Option[String] = Some(id.symbol.name)
-
-    final implicit class IdentifierOps(val id: Identifier) extends AnyVal {
-      def unsafeToSymbolIdentifier: SymbolIdentifier = id.asInstanceOf[SymbolIdentifier]
-    }
+  def apply(sym: Symbol): SymbolIdentifier = {
+    new SymbolIdentifier(FreshIdentifier(sym.path.last), sym)
   }
 
-  implicit class IdentifierWrapper(id: Identifier) {
-    def hasSymbolName(s: String): Boolean = id match {
-      case SymbolIdentifier(`s`) => true
-      case _ => false
-    }
+  def unapply(id: SymbolIdentifier): Option[String] = Some(id.symbol.name)
+
+  final implicit class IdentifierOps(val id: Identifier) extends AnyVal {
+    def unsafeToSymbolIdentifier: SymbolIdentifier = id.asInstanceOf[SymbolIdentifier]
   }
 }
