72d6d57afef8f328ecc9fe19dc695f8eeb79b59d
Jad Hamza <jad.hamza@epfl.ch>
2019-04-10 16:02:02 +0200

    Spacing and adapting benchmarks to new API

--- frontends/benchmarks/smartcontracts/valid/HavocInvariant2.scala
+++ frontends/benchmarks/smartcontracts/valid/HavocInvariant2.scala
@@ -4,35 +4,35 @@ import stainless.annotation._
 import stainless.lang._
 
 trait HavocInvariant2B extends Contract {
-    var isEmpty:Boolean
-    var balance:Uint256
+  var isEmpty:Boolean
+  var balance:Uint256
 
-    final def invariant() = (!isEmpty && (balance > Uint256.ZERO)) ||
-                            (isEmpty && (balance == Uint256.ZERO))
+  final def invariant() = (!isEmpty && (balance > Uint256.ZERO)) ||
+              (isEmpty && (balance == Uint256.ZERO))
 
-    def emptyContract() = {
-        balance = Uint256.ZERO
-        isEmpty = true
-    }
+  def emptyContract() = {
+    balance = Uint256.ZERO
+    isEmpty = true
+  }
 }
 
 trait HavocInvariant2A extends Contract {
-    val target:Address
+  val target:Address
 
-    final def invariant():Boolean = true
+  final def invariant():Boolean = true
 
-    final def withdrawBalance() = {
-        require(
-            Environment.contractAt(target).isInstanceOf[HavocInvariant2B]
-        )
-        // What should we say about the state of the contract B ?
-        // Technically if we can prove that after the constructor has been run, B satisfy
-        // the invariant and if each method satisfy the invariant then the invariant must be
-        // true here.
-        Environment.contractAt[HavocInvariant2B](target).emptyContract()
-        assert(Environment.contractAt[HavocInvariant2B](target).invariant())
-        havoc()
-        assert(Environment.contractAt[HavocInvariant2B](target).invariant())
-    }
+  final def withdrawBalance() = {
+    require(
+      Environment.contractAt(target).isInstanceOf[HavocInvariant2B]
+    )
+    // What should we say about the state of the contract B ?
+    // Technically if we can prove that after the constructor has been run, B satisfy
+    // the invariant and if each method satisfy the invariant then the invariant must be
+    // true here.
+    Environment.contractAt(target).asInstanceOf[HavocInvariant2B].emptyContract()
+    assert(Environment.contractAt(target).asInstanceOf[HavocInvariant2B].invariant())
+    havoc()
+    assert(Environment.contractAt(target).asInstanceOf[HavocInvariant2B].invariant())
+  }
 
 }
--- frontends/benchmarks/smartcontracts/valid/OtherContractMutated.scala
+++ frontends/benchmarks/smartcontracts/valid/OtherContractMutated.scala
@@ -4,25 +4,25 @@ import stainless.annotation._
 import stainless.lang._
 
 trait OCMB extends Contract {
-    var testField1: Uint256
-    val testField2: Uint256
+  var testField1: Uint256
+  val testField2: Uint256
 }
 
 trait OCMA extends Contract {
-    val target:Address
+  val target:Address
 
-    final def invariant() = true
+  final def invariant() = true
 
-    final def foo() = {
-        require(
-            Environment.contractAt(target).isInstanceOf[B]
-        )
+  final def foo() = {
+    require(
+      Environment.contractAt(target).isInstanceOf[OCMB]
+    )
 
-        val oldTestField1 = Environment.contractAt[OCMB](target).testField1
-        val oldTestField2 = Environment.contractAt[OCMB](target).testField2
-        havoc()
-        assert(oldTestField1 == Environment.contractAt[OCMB](target).testField1)
-        assert(oldTestField2 == Environment.contractAt[OCMB](target).testField2)
-    }
+    val oldTestField1 = Environment.contractAt(target).asInstanceOf[OCMB].testField1
+    val oldTestField2 = Environment.contractAt(target).asInstanceOf[OCMB].testField2
+    havoc()
+    assert(oldTestField1 == Environment.contractAt(target).asInstanceOf[OCMB].testField1)
+    assert(oldTestField2 == Environment.contractAt(target).asInstanceOf[OCMB].testField2)
+  }
 
 }
