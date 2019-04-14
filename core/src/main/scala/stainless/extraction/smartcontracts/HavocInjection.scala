7cad9543c8cf77cf9a856e99612b302f9d421ccf
RomainJufer <romain.jufer@epfl.ch>
2019-04-14 15:54:38 +0200

    Modifiy HavocInjection to generate only one Havoc function since we now have a global invariant

--- core/src/main/scala/stainless/extraction/smartcontracts/HavocInjection.scala
+++ core/src/main/scala/stainless/extraction/smartcontracts/HavocInjection.scala
@@ -27,6 +27,16 @@ trait HavocInjection extends oo.SimplePhase
 
     // We collect all contracts
     val contracts = symbols.classes.values.filter(_.isContract)
+    val contractsType = (contracts.map(_.typed.toType) ++ Seq(
+      symbols.lookup[ClassDef]("stainless.smartcontracts.Contract").typed.toType,
+      symbols.lookup[ClassDef]("stainless.smartcontracts.ContractInterface").typed.toType
+    )).toSet[s.Type]
+
+    val globalInvariant = symbols.lookup[FunDef]("environmentInvariant")
+    val envCd = symbols.lookup[ClassDef]("stainless.smartcontracts.Environment")
+    val envType = envCd.typed.toType
+    val contractInterfaceCd = symbols.lookup[ClassDef]("stainless.smartcontracts.ContractInterface")
+    val contractInterfaceType = contractInterfaceCd.typed.toType
 
     // We make sure that contracts are not extended
     contracts.find { cd => !cd.children.isEmpty } match {
@@ -34,10 +44,25 @@ trait HavocInjection extends oo.SimplePhase
       case None => ()
     }
 
+    val envVd = ValDef.fresh("env", envType)
+    val envVar = envVd.toVariable
+    val contractVd = ValDef.fresh("contract", contractInterfaceType)
+    val precondition = Precondition(FunctionInvocation(globalInvariant.id, Nil, Seq(envVar)))
+    val postcondition = Postcondition(Lambda(Seq(ValDef.fresh("res", UnitType())), FunctionInvocation(globalInvariant.id, Nil, Seq(envVar))))
+
+    val havocFd = new FunDef(
+        ast.SymbolIdentifier("havoc"),
+        Seq(),
+        Seq(envVd, contractVd),
+        UnitType(),
+        reconstructSpecs(Seq(precondition, postcondition), Some(NoTree(UnitType())), UnitType()),
+        Seq(Synthetic, Extern, Ghost, IsAbstract)
+      )
+
     // For each contract, we build a `havoc` function that can be used to reset
     // all variables of the contract, while respecting its invariant and
     // evolution functions
-    def buildHavoc(cid: Identifier) = {
+    /*def buildHavoc(cid: Identifier) = {
       new FunDef(
         ast.SymbolIdentifier("havoc"),
         Seq(),
@@ -50,13 +75,36 @@ trait HavocInjection extends oo.SimplePhase
 
     val havocs: Map[Identifier, FunDef] = contracts.map {
       cd => (cd.id, buildHavoc(cd.id))
-    }.toMap
+    }.toMap*/
 
-    override def transform(e: Expr): Expr = e match {
-      case MethodInvocation(t@This(ct), id, tparams, params) if isIdentifier("stainless.smartcontracts.Contract.havoc", id) =>
-        MethodInvocation(t, havocs(ct.id).id, tparams, params).copiedFrom(e)
-      case _ => super.transform(e)
+    override def transform(fd: FunDef): FunDef = {
+      if(fd.isContractMethod) {
+        val ownerType = symbols.classes(fd.findClass.get).typed.toType
+        val envVar = fd.params.collectFirst{ 
+          case v@ValDef(_, tpe, _) if tpe == envType => v.toVariable 
+        }.get
+
+        val nFullBody = postMap {
+          case call@MethodInvocation(receiver, id, tps, args) if symbols.functions(id).isContractMethod =>
+            context.reporter.info(s"Add havoc call at ${call.getPos}")
+            Some(Assert(FunctionInvocation(globalInvariant.id, Seq(), Seq(envVar)), None,
+                        Block(Seq(call),
+                        FunctionInvocation(havocFd.id, Seq(), Seq(envVar, This(ownerType)))).setPos(call.getPos)))
+
+          case call@MethodInvocation(receiver, id, tps, args) if isIdentifier("stainless.smartcontracts.Msg.transfer", id) =>
+            context.reporter.info(s"Add havoc call at ${call.getPos}")
+            Some(Assert(FunctionInvocation(globalInvariant.id, Seq(), Seq(envVar)), None,
+                        Block(Seq(call),
+                        FunctionInvocation(havocFd.id, Seq(), Seq(envVar, This(ownerType)))).setPos(call.getPos)))
+
+          case _ => None
+        } (fd.fullBody)
+
+        super.transform(fd.copy(fullBody = nFullBody))
+      } else super.transform(fd)
     }
+
+    val newFds = Seq(havocFd)
   }
 
   /* ====================================
@@ -64,10 +112,10 @@ trait HavocInjection extends oo.SimplePhase
    * ==================================== */
 
   override def extractSymbols(context: TransformerContext, symbols: Symbols): Symbols = {
-    val oldHavoc = symbols.lookup.get[FunDef]("stainless.smartcontracts.Contract.havoc").map(_.id).toSet
-    val newSymbols = super.extractSymbols(context, symbols.withFunctions(context.havocs.values.toSeq))
+    //val oldHavoc = symbols.lookup.get[FunDef]("stainless.smartcontracts.Contract.havoc").map(_.id).toSet
+    val newSymbols = super.extractSymbols(context, symbols.withFunctions(context.newFds))
     NoSymbols.withFunctions(
-      newSymbols.functions.values.toSeq.filterNot { fd => oldHavoc.contains(fd.id) }
+      newSymbols.functions.values.toSeq //.filterNot { fd => oldHavoc.contains(fd.id) }
     ).withClasses(
       newSymbols.classes.values.toSeq
     )
