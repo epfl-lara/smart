29dcc892a858ad04226f667fc4989789a62fdb64
RomainJufer <romain.jufer@epfl.ch>
2019-04-14 09:31:56 +0200

    Add EtherUpdate. The method is correctly constructed but is not verified at the moment. Need to fix this

---
+++ core/src/main/scala/stainless/extraction/smartcontracts/EtherUpdateInjection.scala
@@ -0,0 +1,82 @@
+/* Copyright 2009-2018 EPFL, Lausanne */
+
+package stainless
+package extraction
+package smartcontracts
+import scala.collection.mutable.{Map => MutableMap, Set => MutableSet}
+
+trait EtherUpdateInjection extends oo.SimplePhase
+  with oo.SimplyCachedClasses
+  with SimplyCachedSorts
+  with SimplyCachedFunctions
+  { self =>
+  val s: trees.type
+  val t: s.type
+  import s._
+
+  /* ====================================
+   *       Context and caches setup
+   * ==================================== */
+
+  override protected def getContext(symbols: s.Symbols) = new TransformerContext()(symbols)
+  protected class TransformerContext(implicit val symbols: s.Symbols) extends oo.TreeTransformer {
+    val s: self.s.type = self.s
+    val t: self.t.type = self.t
+
+    import s.exprOps._
+
+    val globalInvariant = symbols.lookup[FunDef]("environmentInvariant")
+    val addressCd: ClassDef = symbols.lookup[ClassDef]("stainless.smartcontracts.Address")
+    val addressType: ClassType = addressCd.typed.toType
+    val envCd: ClassDef = symbols.lookup[ClassDef]("stainless.smartcontracts.Environment")
+    val envType: ClassType = envCd.typed.toType
+    val balanceAccessor: Identifier = envCd.fields.find(vd => isIdentifier("stainless.smartcontracts.Environment.balances", vd.id)).get.id
+
+    val envVd = ValDef.fresh("env", envType)
+    val addrVd = ValDef.fresh("addr", addressType)
+
+    val uone = BVLiteral(false, 1, 256)
+
+    val etherUpdateBody = Block(Seq(MutableMapUpdate(
+                            ClassSelector(envVd.toVariable, balanceAccessor),
+                            addrVd.toVariable,
+                            Plus(
+                                MutableMapApply(ClassSelector(envVd.toVariable, balanceAccessor), addrVd.toVariable),
+                                uone
+                            ))), NoTree(UnitType()))
+
+    val preCondition = Precondition(FunctionInvocation(globalInvariant.id, Nil, Seq(envVd.toVariable)))
+    val postCondition = Postcondition(Lambda(Seq(ValDef.fresh("res", UnitType())), FunctionInvocation(globalInvariant.id, Nil, Seq(envVd.toVariable))))
+    val fdBody = reconstructSpecs(Seq(preCondition, postCondition), Some(etherUpdateBody), UnitType())
+
+    val fd = new FunDef(
+        ast.SymbolIdentifier("etherUpdate"),
+        Seq(),
+        Seq(envVd, addrVd),
+        UnitType(),
+        fdBody,
+        Seq(Synthetic, Extern)
+    )
+    
+    val newFds = Seq(fd)
+  }
+
+  /* ====================================
+   *             Extraction
+   * ==================================== */
+
+  override def extractSymbols(context: TransformerContext, symbols: Symbols): Symbols = {
+    super.extractSymbols(context, symbols.withFunctions(context.newFds.toSeq))
+  }
+}
+
+object EtherUpdateInjection {
+  def apply()(implicit ctx: inox.Context): ExtractionPipeline {
+    val s: trees.type
+    val t: trees.type
+  } = new {
+    override val s: trees.type = trees
+    override val t: trees.type = trees
+    override val context = ctx
+  } with EtherUpdateInjection
+}
