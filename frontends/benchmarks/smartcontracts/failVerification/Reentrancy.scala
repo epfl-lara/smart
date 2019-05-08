0deea6abc23f4e77ddc3619822d84c207b4fe789
Jad Hamza <jad.hamza@epfl.ch>
2019-05-08 13:19:30 +0200

    Temporarily remove FalseInvariant, Voting, MinimumToken, LoanContract, GhostUpdate, CallWithEther1 examples

--- frontends/benchmarks/smartcontracts/failVerification/Reentrancy.scala
+++ frontends/benchmarks/smartcontracts/failVerification/Reentrancy.scala
@@ -1,47 +1,33 @@
-// FIXME : Use old syntax
-
 import stainless.smartcontracts._
-import stainless.collection._
 import stainless.lang._
+import stainless.collection._
 import stainless.annotation._
 
-trait Reentrancy extends Contract {
-   var balanceOf: MutableMap[Address,Uint256]
-
-   // Only called when the contract is created, for initialization
-   def deposit(): Unit = {
-      require(
-        Msg.value >= Uint256.ZERO
-      )
-
-      balanceOf(Msg.sender) = balanceOf(Msg.sender) + Msg.value
-   } ensuring (
-      old(this).balanceOf(Msg.sender) == balanceOf(Msg.sender) + Msg.value
-   )
-
-   def withdraw(): Unit = {
-      require(
-        Msg.value >= Uint256.ZERO
-      )
-
-      if(balanceOf(Msg.sender) >= Msg.value) {
-        withdraw() // ???
-        balanceOf(Msg.sender) = balanceOf(Msg.sender) - Msg.value
-      }
-   } ensuring {
-      Proof.withdrawLemma(old(this), this, Msg.value, Msg.sender)
-   }
+trait RB extends Contract {
+  def doSomething(): Unit
 }
 
-object Proof {
-   def withdrawLemma(
-      old: Reentrancy,
-      neww: Reentrancy,
-      amount: Uint256,
-      from: Address
-   ) = {
-      (old.balanceOf(from) >= amount ==> (neww.balanceOf(from) == old.balanceOf(from) - amount)) &&
-      (old.balanceOf(from) < amount ==> (neww.balanceOf(from) == neww.balanceOf(from)))
-   }
-}
+trait RA extends Contract {
+  var userBalance: Uint256
+  var contractBalance: Uint256
+  var totalCoins: Uint256
 
+  @addressOfContract("RB")
+  val target: Address
+
+  final def invariant() = userBalance + contractBalance == totalCoins
+
+  final def withdrawBalance() = {
+    val amount = userBalance
+
+    // Changing the state here would be ok
+    // totalCoins = totalCoins - amount
+    // userBalance = Uint256.ZERO
+
+    Environment.contractAt(target).asInstanceOf[RB].doSomething
+
+    // Shouldn't work to change the state here
+    totalCoins = totalCoins - amount
+    userBalance = Uint256.ZERO
+  }
+}
