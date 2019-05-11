5fb61013f515f9b4d9e4549804d7221cad84eb1d
RomainJufer <romain.jufer@epfl.ch>
2019-05-11 13:42:01 +0200

    Add self-ref examples

---
+++ frontends/benchmarks/smartcontracts/failVerification/SelfReference.scala
@@ -0,0 +1,17 @@
+import stainless.smartcontracts._
+import stainless.annotation._
+
+trait SRDA extends Contract {
+  var x: Uint256
+
+  @addressOfContract("SRDA")
+  val other: Address
+
+  def foo() = {
+    val old = x
+    Environment.contractAt(other).asInstanceOf[SRDA].increment()
+    assert(old == x)
+  }
+
+  def increment() = x = x + Uint256.ONE
+}
