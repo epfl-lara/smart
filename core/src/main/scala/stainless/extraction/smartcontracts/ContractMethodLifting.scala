3b34f13e860a58dd682897bcf554bbda0b8fa078
RomainJufer <romain.jufer@epfl.ch>
2019-06-04 20:28:44 +0200

    Fix injection of callee cast in invariant

--- core/src/main/scala/stainless/extraction/smartcontracts/ContractMethodLifting.scala
+++ core/src/main/scala/stainless/extraction/smartcontracts/ContractMethodLifting.scala
@@ -76,18 +76,20 @@ trait ContractMethodLifting extends oo.SimplePhase
         val calleeAddrEquality = Equals(calleeVd.toVariable,
                                  MethodInvocation(AsInstanceOf(calleeContract, contractType), addrFieldId, Seq(), Seq()))
 
-        // If fd is the constructor we still need to add the require on the callee as the constructor will be lifted out.
-        val newPre = if(fd.isConstructor) Precondition(And(calleeIsInstanceOf, calleeAddrEquality))
-                     else pre
+        
+        val body = bodyOpt.getOrElse(NoTree(fd.returnType))
 
-        val newBodyOpt = bodyOpt.map(body =>
-          if (fd.isContractMethod) body else And(And(calleeIsInstanceOf, calleeAddrEquality), body)
-        )
+        val newBody = if(!fd.isInvariant) {
+          val newPre = Precondition(And(calleeIsInstanceOf, calleeAddrEquality))
+          reconstructSpecs(Seq(newPre, post), Some(body), fd.returnType)
+        } else {
+          And(And(calleeIsInstanceOf, calleeAddrEquality), body)
+        }
 
         super.transform(fd.copy(
           flags = fd.flags.filterNot{ case IsMethodOf(_) => true case _ => false},
           params = newParams,
-          fullBody = reconstructSpecs(Seq(newPre, post), newBodyOpt, fd.returnType)
+          fullBody = newBody
         ).copiedFrom(fd))
 
       case fd => super.transform(fd)
